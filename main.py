1913dc1f94cb44082c794066e56ae21be5b416b1
Leroy Salih <leroy.salih@gmail.com>
2020-12-07 01:51:06 +0000

    Uploading work.

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#insert your code here.
+h = 0
+index = 0
+items= [3, 5, 1, 2]
+
+while index < len(items):
+    if items[index] > h:
+        h = items[index]
+    index = index + 1
+
+print ("h is ", h)
